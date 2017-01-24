3fc1f8baa10549926b0c9d250cac5824585079df
seannam1218 <seannam1218@gmail.com>
2017-01-24 15:04:28 -0800

    Props passed from parent to child

--- components/App.js
+++ components/App.js
@@ -2,43 +2,81 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var App = React.createClass({
-	
-	handleChange: function(e) {
-		console.log(e.target);
-	},
-	
-	handleSubmit: function (e) {
+
+	handleDeleteClick: function (e) {
 		e.preventDefault();
-		var value = e.target;
-		console.log("submitted: " + value);
-		p = document.createElement("p");
-		p.innerHTML = "wowomg"
-		document.getElementById("app").appendChild(p);
+		var confirmDelete = confirm("Are you sure you want to delete every item on the list?");
+		if (confirmDelete) {
+			document.getElementById('list').innerHTML = "";
+		}
 	},
+
+	handleSaveClick: function (e) {
+		e.preventDefault();
+		console.log("save clicked")
+	},
+	
+	/*handleSubmit: function (e) {
+		e.preventDefault();
+		//var value = e.target.childNodes[4].value;
+		var value = document.getElementsByClassName("form-input")[0].value
+		p = document.createElement("p");
+		p.innerHTML = "- " + value;
+		document.getElementById("list").appendChild(p);
+	},*/
 	
 	render: function () {
 		return (
-			<Form 
-				description = "Add items" 
-				onSubmit = {this.handleSubmit}
-				onChange = {this.handleChange}
-			/>
+			<div>
+				<Form 
+					description = "Add a new item" 
+					//onSubmit = {this.handleSubmit}
+				/>
+				<DeleteAllButton onDeleteClick = {this.handleDeleteClick} />
+				<SaveAllButton onSaveClick = {this.handleSaveClick} />
+				<div id = "list"></div>
+			</div>
 		)
 	}
 });
 
 var Form = React.createClass({
+	
+	handleSubmit: function (e) {
+		e.preventDefault();
+		//var value = e.target.childNodes[4].value;
+		var value = document.getElementsByClassName("form-input")[0].value
+		p = document.createElement("p");
+		p.innerHTML = "- " + value;
+		document.getElementById("list").appendChild(p);
+	},
+	
 	render: function () {
 		return (
-			<form onSubmit = {this.props.onSubmit}>
+			<form onSubmit = {this.handleSubmit}>
 				{this.props.description}<br />
-				<input type = "text" onChange = {this.props.onChange} />
+				<input className = "form-input" type = "text" />
 				<input type = "submit" value = "Submit" />
 			</form>
 		)
 	}
 })
 
+var DeleteAllButton = React.createClass({
+	render: function () {
+		return (
+			<button onClick = {this.props.onDeleteClick}>Delete all</button>
+		)
+	}
+})
+
+var SaveAllButton = React.createClass ({
+	render: function () {
+		return (
+			<button onClick = {this.props.onSaveClick}>Save work</button>
+		)
+	}
+})
 
 
 module.exports = App;
