df8e0f61e1c5b905b1d12244e80de386dcbb3b65
seannam1218 <seannam1218@gmail.com>
2017-01-24 13:14:31 -0800

    Form onSubmit testing
    onSubmit is working, but have not figured out how to select the form's
    input value

--- components/App.js
+++ components/App.js
@@ -1,15 +1,44 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Form = require('./components/Form');
 
 var App = React.createClass({
+	
+	handleChange: function(e) {
+		console.log(e.target);
+	},
+	
+	handleSubmit: function (e) {
+		e.preventDefault();
+		var value = e.target;
+		console.log("submitted: " + value);
+		p = document.createElement("p");
+		p.innerHTML = "wowomg"
+		document.getElementById("app").appendChild(p);
+	},
+	
 	render: function () {
 		return (
-			<div>
-				<Form />
-			</div>
+			<Form 
+				description = "Add items" 
+				onSubmit = {this.handleSubmit}
+				onChange = {this.handleChange}
+			/>
 		)
 	}
 });
+
+var Form = React.createClass({
+	render: function () {
+		return (
+			<form onSubmit = {this.props.onSubmit}>
+				{this.props.description}<br />
+				<input type = "text" onChange = {this.props.onChange} />
+				<input type = "submit" value = "Submit" />
+			</form>
+		)
+	}
+})
+
+
 
 module.exports = App;
