1876d46bfdacf370599abc403041ab5a73f44365
seannam1218 <seannam1218@gmail.com>
2017-02-04 05:37:05 -0800

    edit and delete item functions added, but save malfunction
    save malfunctions. When list is edited, it saves automatically for no
    apparent reason.

--- components/App.js
+++ components/App.js
@@ -6,8 +6,6 @@ var App = React.createClass({
 		return {
 			currentItems: [],
 			savedItems: [],
-			deleteIcons: [],
-			editIcons: []
 		}
 	},
 	
@@ -19,27 +17,69 @@ var App = React.createClass({
 	},
 	
 	handleDeleteAll: function (e) {
+		var newBlankList = this.makeBlankCurrentItems()
 		this.setState ({
-			currentItems: []
+			currentItems: newBlankList
 		})
 	},
 	
+	makeBlankCurrentItems: function() {
+		for (var i = 0; i < this.state.currentItems.length; i++) {
+			this.state.currentItems.splice(0)
+		}
+		return this.state.currentItems
+	},
+	
 	handleSaveAll: function (e) {
+		var newSavedItems = this.state.currentItems
 		this.setState ({
-			savedItems: this.state.currentItems
+			savedItems: newSavedItems
 		})
 	},
 	
 	handleLoad: function (e) {
+		var newCurrentItems = this.state.savedItems
 		this.setState ({
-			currentItems: this.state.savedItems
+			currentItems: newCurrentItems
 		})
 	},
 	
-	handleDeleteItem: function (e) {
-		/*this.setState ({
-			currentItems: []
-		})*/
+	handleDeleteItem: function (e, target) {
+		var newCurrentItems = this.makeDeletedCurrentItems(e, target)
+		this.setState ({
+			currentItems: newCurrentItems
+		})
+	},
+	
+	makeDeletedCurrentItems: function (e, target) {
+		for (var i = 0; i < this.state.currentItems.length; i++) {
+			if (this.state.currentItems[i] === target) {
+				this.state.currentItems.splice(i, 1)
+			} 
+		} 
+		return this.state.currentItems
+	},
+	
+	handleEditItem: function (e, target) {
+		var editPrompt = prompt("How would you like to change it?", target)
+		if (this.state.currentItems.includes(editPrompt)) {
+			alert(editPrompt + ": This item already exists in the list!")
+			return
+		} else if (editPrompt !== target) {
+			var newCurrentItems = this.makeEditedCurrentItems(target, editPrompt)
+			this.setState ({
+				currentItems: newCurrentItems
+			})
+		} 
+	},
+	
+	makeEditedCurrentItems: function(target, editPrompt) {
+		for (var i = 0; i < this.state.currentItems.length; i++) {
+			if (this.state.currentItems[i] == target) {
+				this.state.currentItems.splice(i, 1, editPrompt)
+			} 
+		}	
+		return this.state.currentItems
 	},
 	
 	componentDidUpdate: function (prevProps, prevState) {
@@ -50,11 +90,11 @@ var App = React.createClass({
 	render: function () {
 		return (
 			<div>
-				<Form onSubmit = {this.addItem} />
+				<Form currentItems = {this.state.currentItems} onSubmit = {this.addItem} />
 				<DeleteAllButton onClick = {this.handleDeleteAll} />
 				<SaveAllButton onClick = {this.handleSaveAll} savedItems = {this.state.savedItems} />
 				<LoadButton onClick = {this.handleLoad} />
-				<List currentItems = {this.state.currentItems} onDelete = {this.handleDeleteItem} />
+				<List currentItems = {this.state.currentItems} onDelete = {this.handleDeleteItem} onEdit = {this.handleEditItem} />
 			</div>
 		)
 	}
@@ -69,7 +109,6 @@ var Form = React.createClass({
 	
 	description: "Add a new item",
 	
-	
 	handleValueChange: function (e) {
 		this.setState ({
 			value: e.target.value
@@ -79,6 +118,10 @@ var Form = React.createClass({
 	handleSubmit: function (e) {
 		e.preventDefault();
 		if (this.state.value !== "") {
+			if (this.props.currentItems.includes(this.state.value)) {
+				alert(this.state.value + ": This item is already in the list!")
+				return
+			}
 			this.props.onSubmit(this.state.value);
 		} else {
 			alert("Item must be at least 1 character long! Why would you add a blank item???")
@@ -89,7 +132,7 @@ var Form = React.createClass({
 		return (
 			<form onSubmit = {this.handleSubmit}>
 				{this.description}<br />
-				<input id = "form-input" type = "text" onChange = {this.handleValueChange}/>
+				<input type = "text" onChange = {this.handleValueChange}/>
 				<input type = "submit" value = "Submit" />
 			</form>
 		)
@@ -97,27 +140,23 @@ var Form = React.createClass({
 })
 
 var List = React.createClass({
-	/*makeListItems: function () {
-		if (this.props.currentItems !== null) {
-			var itemPs = this.props.currentItems.map(function (item) {
-				return (
-					<li>
-						{item} <DeleteIcon onClick = {this.handleDelete} />
-					</li>
-				)
-			})
-			return (itemPs)	
-		}
-		return []
-	},*/
-	handleDelete : function () {
-		this.props.onDelete();
+	handleDeleteItem : function (e, target) {
+		this.props.onDelete(e, target);
+	},
+	
+	handleEditItem : function (e, target) {
+		this.props.onEdit(e, target);
 	},
 	
 	makeListItems : function () {
-		var listItems = this.props.currentItems.map(function (item) {
+		var s = " "
+		var listItems = this.props.currentItems.map((item) => {
 			return (
-				<ListItem key={item.id} value={item} onDelete = {this.handleDelete} />
+				<li key={item.id} id={item} > 
+					{item}{s} 
+					<EditIcon onEdit = {this.handleEditItem} />
+					<DeleteIcon onDelete = {this.handleDeleteItem} />
+				</li>
 			)
 		})
 		return listItems
@@ -132,27 +171,14 @@ var List = React.createClass({
 	}
 })
 
-var ListItem = React.createClass({
-	handleDelete: function (e) {
-		this.props.onDelete()
-	},
-	
-	render: function () {
-		return (
-			<li> 
-				{this.props.value} 
-				<DeleteIcon onClick = {this.handleDelete} />
-			</li>
-		)
-	}
-})
-
 var DeleteIcon = React.createClass ({
 	deleteIconSrc: "https://cdn3.iconfinder.com/data/icons/google-material-design-icons/48/ic_delete_48px-128.png",
 	iconWidth: 20,
 	
 	handleClick: function (e) {
-		this.props.onClick(e)
+		var target = e.target.parentElement.id
+		console.log(target)
+		this.props.onDelete(e, target)
 	},
 	
 	render: function() {
@@ -167,7 +193,10 @@ var EditIcon = React.createClass ({
 	iconWidth: 20,
 		
 	handleClick: function (e) {
-		var target = e.target
+		console.log("clicked! - EditIcon")
+		var target = e.target.parentElement.id
+		console.log(target)
+		this.props.onEdit(e, target)
 	},
 	
 	render: function() {
@@ -232,6 +261,5 @@ var LoadButton = React.createClass ({
 		)
 	}
 })
-
 
 module.exports = App;
